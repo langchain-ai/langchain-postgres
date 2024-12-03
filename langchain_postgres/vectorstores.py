8acc01b64986000fa1335d93651e0f823303a641
Daniel Fornarini <daniel@monade.io>
2024-12-03 17:28:25 +0100

    :fix: binary quantization

--- langchain_postgres/vectorstores.py
+++ langchain_postgres/vectorstores.py
@@ -1689,7 +1689,7 @@ class PGVector(VectorStore):
         return (
             select(
                 embedding_store_bundle,
-                embedding_store_bundle.c.embedding.binary_distance(embedding).label("distance"),
+                embedding_store_bundle.embedding.binary_distance(embedding).label("distance"),
             )
             .order_by(sqlalchemy.asc("distance"))
             .limit(k)
