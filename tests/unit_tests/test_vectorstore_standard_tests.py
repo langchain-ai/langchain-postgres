34adeb01724d323b449df83517274b662a53b71f
Chester Curme <chester.curme@gmail.com>
2024-12-09 16:06:19 -0500

    update

--- tests/unit_tests/test_vectorstore_standard_tests.py
+++ tests/unit_tests/test_vectorstore_standard_tests.py
@@ -1,13 +1,13 @@
-from typing import Generator
+from typing import AsyncGenerator, Generator
 
 import pytest
 from langchain_core.vectorstores import VectorStore
 from langchain_tests.integration_tests import VectorStoreIntegrationTests
 
-from tests.unit_tests.test_vectorstore import get_vectorstore
+from tests.unit_tests.test_vectorstore import aget_vectorstore, get_vectorstore
 
 
-class TestStandard(VectorStoreIntegrationTests):
+class TestSync(VectorStoreIntegrationTests):
     @pytest.fixture()
     def vectorstore(self) -> Generator[VectorStore, None, None]:  # type: ignore
         """Get an empty vectorstore for unit tests."""
@@ -16,3 +16,22 @@ class TestStandard(VectorStoreIntegrationTests):
             vstore.create_tables_if_not_exists()
             vstore.create_collection()
             yield vstore
+
+    @property
+    def has_async(self) -> bool:
+        return False
+
+
+class TestAsync(VectorStoreIntegrationTests):
+    @pytest.fixture()
+    async def vectorstore(self) -> AsyncGenerator[VectorStore, None]:  # type: ignore
+        """Get an empty vectorstore for unit tests."""
+        async with aget_vectorstore(embedding=self.get_embeddings()) as vstore:
+            await vstore.adrop_tables()
+            await vstore.acreate_tables_if_not_exists()
+            await vstore.acreate_collection()
+            yield vstore
+
+    @property
+    def has_sync(self) -> bool:
+        return False
